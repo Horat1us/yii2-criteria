6b2902a2b4717db19fc14a2ca44eaa98852bf25e
Alexander Letnikow <reclamme@gmail.com>
2018-01-16 12:02:33 +0200

    Add column quoting in SelectCriteria

--- src/SelectCriteria.php
+++ src/SelectCriteria.php
@@ -6,6 +6,7 @@ use Horat1us\Yii\Criteria\Interfaces\CriteriaInterface;
 use yii\base\Model;
 use yii\db\ActiveQuery;
 use yii\db\ActiveRecord;
+use yii\db\Connection;
 use yii\db\Query;
 
 /**
@@ -16,6 +17,15 @@ class SelectCriteria extends Model implements CriteriaInterface
 {
     /** @var string[] */
     public $fields;
+
+    /** @var Connection */
+    protected $connection;
+
+    public function __construct(Connection $connection, array $config = [])
+    {
+        parent::__construct($config);
+        $this->connection = $connection;
+    }
 
     public function rules()
     {
@@ -36,11 +46,17 @@ class SelectCriteria extends Model implements CriteriaInterface
             $fields = array_filter(
                 $fields,
                 function (string $field) use ($attributes) {
-                    return in_array($field, $attributes);
+                    return in_array($field, $attributes)
+                        || in_array(explode(".", $field, 2)[1] ?? $field, $attributes);
                 }
             );
         }
 
-        return $query->select($fields);
+        return $query->select(array_map(
+            function (string $field) {
+                return $this->connection->schema->quoteSimpleColumnName($field);
+            },
+            $fields
+        ));
     }
 }
